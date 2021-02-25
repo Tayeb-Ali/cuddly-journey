8b202015a7d6b2d7a1ec25bc31560a8238a32281
Elteyab Hassan <Elteyab.Hassan@gmail.com>
2021-02-25 17:05:40 +0200

    update models

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,21 +41,22 @@ namespace Saidality.Controllers
             //              select c).ToList();
 
             var result = (from ep in _auc.Stocks
-                          join e in _auc.Locaton on ep.StockID equals e.LocatonID 
+                          join e in _auc.Locaton on ep.StockID equals e.LocatonID
                           join me in _auc.Medicines on ep.StockID equals me.MedicineID
                           join ph in _auc.Pharmcies on ep.StockID equals ph.PharmcyID
                           where e.LocatonID == LocatonID && ep.Mediciene.BrandName.Contains(BrandName) || ep.Mediciene.ScientificName.Contains(BrandName)
                           select new
                           {
-                              medicineID= ep.Mediciene.MedicineID,
-                              BrandName = ep.Mediciene.BrandName,
-                              ScientificName= ep.Mediciene.ScientificName,
-                              Type= ep.Mediciene.Type,
-                              price= ep.Mediciene.Price,
-                              pharmcies= ep.Pharmcy,
-                              State = e.State,
-                          }).Take(10);
-            //return Ok(result);
+                              //MedicineID = ep.Mediciene.MedicineID,
+                              //BrandName = ep.Mediciene.BrandName,
+                              //ScientificName = ep.Mediciene.ScientificName,
+                              //Type = ep.Mediciene.Type,
+                              ep.Mediciene,
+                              
+                              //pharmcies = ep.Pharmcy,
+                              //State = e.State,
+                          }).ToList();
+            return Ok(result);
 
             if (result == null)
             {
---
+++ ViewModel/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Saidality.Models;
+
+namespace Saidality.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Medicine> GetMedicines { get; set; }
+    }
+}
