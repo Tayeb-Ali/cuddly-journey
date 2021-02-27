e645d36ed63f20c4773eee060822587a89c78848
Elteyab Hassan <Elteyab.Hassan@gmail.com>
2021-02-27 09:13:43 +0200

    Update

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Saidality.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index(){
+return View();
+        }
+    }
+}
